27b3de4036737ac596985045ec71da8948757248
Barkın Zorlu <barkin.zorlu.bz@gmail.com>
2026-05-13 00:31:55 +0300

    Update New Styles and Themes

--- assets/scripts/GUI/end_game_popup.gd
+++ assets/scripts/GUI/end_game_popup.gd
@@ -2,22 +2,20 @@ extends CenterContainer
 
 @export var panel : PanelContainer
 
-@export var won_label : Label
-@export var lost_label : Label
+@onready var won_panel : PanelContainer = $"PanelContainer/MarginContainer/VBoxContainer/Won PanelContainer"
+@onready var lost_panel : PanelContainer = $"PanelContainer/MarginContainer/VBoxContainer/Lost PanelContainer"
 
 func game_won():
 	GameManager.slow_time_scale()
 	self.visible = true
-	panel.self_modulate = Color.GREEN
-	lost_label.visible = false
-	won_label.visible = true
+	lost_panel.visible = false
+	won_panel.visible = true
 
 func game_lost():
 	GameManager.slow_time_scale()
 	self.visible = true
-	panel.self_modulate = Color.RED
-	won_label.visible = false
-	lost_label.visible = true
+	won_panel.visible = false
+	lost_panel.visible = true
 
 func on_return_button():
 	GameManager.normal_time_scale()
