3cd99e0bc2f96d1d3292753bc46e66c552395c2a
Barkın Zorlu <barkin.zorlu.bz@gmail.com>
2026-05-14 03:17:58 +0300

    Update main_menu.gd

--- assets/scripts/GUI/main_menu.gd
+++ assets/scripts/GUI/main_menu.gd
@@ -5,8 +5,14 @@ class_name MainMenu extends Control
 
 @onready var highest_score: HighestScoreGUI = $"PanelContainer/MarginContainer/Left Side VSplitContainer/Highest Score"
 
+@onready var exit_button: Button = $"PanelContainer/MarginContainer/Left Side VSplitContainer/Buttons VBoxContainer/Exit Button"
+
 func _init() -> void:
 	GameManager.main_menu = self
+
+func _ready() -> void:
+	if OS.has_feature("web"):
+		exit_button.text = "Reset"
 
 func on_play_pressed():
 	SceneLoader.load_direct(game_scene.resource_path)
