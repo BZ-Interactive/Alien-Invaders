a63ff3874100bed7ca14f6bc42714716d1981186
ZorluBarkin <barkin.zorlu.bz@gmail.com>
2026-05-23 16:36:31 +0300

    Add health and powerup bar to player

--- assets/scripts/player_ship.gd
+++ assets/scripts/player_ship.gd
@@ -1,46 +1,55 @@
 class_name Player extends CharacterBody2D
 
-@export var projectile : PackedScene
+@export var projectile: PackedScene
 const shooting_direction = Vector2.UP
  # better to use timer as its handled in Input
-@onready var fire_cooldown_timer : Timer = $"Fire Cooldown Timer"
-var can_fire : bool = false
+@onready var fire_cooldown_timer: Timer = $"Fire Cooldown Timer"
+var can_fire: bool = false
 
 # powerup variables
-@onready var powerup_timer : Timer = $"Powerup Timer"
-var current_power : String
-var firerate_up : bool = false
-var shield_up : bool = false
-var speed_mult : float = 1 # 1 means no active power for speed
+@onready var powerup_timer: Timer = $"Powerup Timer"
+var current_power: String
+var firerate_up: bool = false
+var shield_up: bool = false
+var speed_mult: float = 1 # 1 means no active power for speed
+
+# powerup bar and styles
+@onready var powerup_bar: ProgressBar = $"Powerup Bar"
+@export var speed_style: StyleBoxFlat
+@export var shield_style: StyleBoxFlat
+@export var firerate_style: StyleBoxFlat
 
 const SPRITE_SIZE = 50
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-@export var health : float = 3.0
+@export var health: float = 3.0
+@onready var health_bar: ProgressBar = $"Health Bar"
 
-@onready var shield_sprite : Sprite2D = $"Shield Sprite2D"
+@onready var shield_sprite: Sprite2D = $"Shield Sprite2D"
 
-@onready var idle_visual : Sprite2D = $"Ship idle Sprite2D"
-@onready var left_visual : Sprite2D = $"Ship Left Sprite2D"
-@onready var right_visual : Sprite2D = $"Ship Right Sprite2D"
+@onready var idle_visual: Sprite2D = $"Ship idle Sprite2D"
+@onready var left_visual: Sprite2D = $"Ship Left Sprite2D"
+@onready var right_visual: Sprite2D = $"Ship Right Sprite2D"
 
 @export_group("Sprites")
 @export_subgroup("Standard")
-@export var idle_standard : CompressedTexture2D
-@export var left_standard : CompressedTexture2D
-@export var right_standard : CompressedTexture2D
+@export var idle_standard: CompressedTexture2D
+@export var left_standard: CompressedTexture2D
+@export var right_standard: CompressedTexture2D
 
 @export_subgroup("Powered Up")
-@export var idle_powered : CompressedTexture2D
-@export var left_powered : CompressedTexture2D
-@export var right_powered : CompressedTexture2D
+@export var idle_powered: CompressedTexture2D
+@export var left_powered: CompressedTexture2D
+@export var right_powered: CompressedTexture2D
 
 var dead : bool = false
 
 func _ready() -> void:
 	fire_cooldown_timer.timeout.connect(on_fire_cooldown_timeout)
 	powerup_timer.timeout.connect(on_powerup_timeout)
+	health_bar.value = self.health
+	powerup_bar.visible = false
 
 func _input(event: InputEvent) -> void:
 	if not check_input_blocked() and (event.is_action_pressed("ui_accept") or event.is_action_pressed("ui_up")):
@@ -67,6 +76,10 @@ func _physics_process(_delta: float) -> void:
 	var screen_size = get_viewport_rect().size
 	position.x = clamp(position.x, -(screen_size.x / 2) + SPRITE_SIZE, screen_size.x / 2 - SPRITE_SIZE)
 
+func _process(_delta: float) -> void:
+	if powerup_bar.visible:
+		powerup_bar.value = powerup_timer.time_left
+
 func power_up(power_type : String, time : float) -> void:
 	reset_powerup()
 	if power_type == "firerate":
@@ -74,16 +87,21 @@ func power_up(power_type : String, time : float) -> void:
 		idle_visual.texture = idle_powered
 		left_visual.texture = left_powered
 		right_visual.texture = right_powered
+		powerup_bar.add_theme_stylebox_override("fill", firerate_style)
 	elif power_type == "speed":
 		speed_mult = 2
 		modulate = Color.GREEN_YELLOW
+		powerup_bar.add_theme_stylebox_override("fill", speed_style)
 	elif power_type == "shield":
 		shield_up = true
 		shield_sprite.visible = true
+		powerup_bar.add_theme_stylebox_override("fill", shield_style)
 		
 	current_power = power_type
 	powerup_timer.wait_time = time
 	powerup_timer.start()
+	powerup_bar.max_value = time
+	powerup_bar.visible = true
 	# can add effects
 
 func change_movement_sprite(direction : float):
@@ -110,6 +128,7 @@ func shoot() -> void:
 		fire_cooldown_timer.start()
 
 func reset_powerup():
+	powerup_bar.visible = false
 	idle_visual.texture = idle_standard
 	left_visual.texture = left_standard
 	right_visual.texture = right_standard
@@ -134,6 +153,7 @@ func damage(dmg : float) -> void:
 	if shield_up:
 		return
 	self.health -= dmg
+	health_bar.value = self.health
 	if health <= 0:
 		die()
 
